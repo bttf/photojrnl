6bca06f1a59565fe4e10983911d8b24e5de7ea80
bttf <adnanchowdhury88@gmail.com>
2015-05-09 11:44:41 -0400

    profile fixes

--- ember-app/app/controllers/profile.js
+++ ember-app/app/controllers/profile.js
@@ -1,10 +1,19 @@
 import Ember from 'ember';
 
+var $ = Ember.$;
+var moment = window.moment;
+
 export default Ember.Controller.extend({
   actions: {
     save: function(profile) {
+      var self = this;
       profile.save().then(function() {
-        console.log('debug: profile saved');
+        var $savedMsg = $('._profile .saved-msg');
+        self.set('saved', 'Saved @ ' + moment().format('MMMM Do YYYY, h:mm:ss a'));
+        $savedMsg.show();
+        setTimeout(function() {
+          $savedMsg.fadeOut(500);
+        }, 2000);
       }, function(err) {
         console.log('debug: profile not saved');
       });
